e2ba17aa69162c10b4539054d79e35db8414c6bd
Chinmay Gandhewar <134136784+Chinu1712@users.noreply.github.com>
2025-08-10 17:18:36 +0530

    selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,1 +1,28 @@
-
+void selection_sort(vector<int>&nums){
+  int n = nums.size();
+  for(int i=0;i<n;i++){
+    int mini = i;
+    for(int j=0;j<n;j++){
+      if(nums[mini] > nums[j]){
+       mini = j;
+      }
+      int temp = nums[mini];
+      nums[mini] = nums[i];
+      nums[i] = temp;
+  }
+}
+}
+int main(){
+  int size;
+cout<<"Enter the size of the array : ";
+cin>>size;
+vector<int>nums(size);
+for(int i=0;i<size;i++){
+cout<<"Enter the element : ",i+1;
+cin>>nums[i];
+}
+selection_sort(nums);
+for(int i=0;i<size;i++){
+cout<<nums[i]<<" ";
+}
+}
